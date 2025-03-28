6f9126f5efb9a91b824361eecd61c6254621e8df
Robonics <ezj213@gmail.com>
2025-03-27 22:29:30 -0400

    initial physobj def

---
+++ src/include/phys_object.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "box2d/box2d.h"
+#include <SFML/System.hpp>
+#include <variant>
+
+namespace Arcade {
+	struct PhysSettings {
+		b2BodyDef def = b2DefaultBodyDef();
+
+		size_t shape_count;
+		b2ShapeDef* shapes;
+		std::variant<
+			b2Polygon,
+			b2Circle,
+			b2Capsule,
+			b2Segment,
+			b2ChainSegment
+		>* shape_data;
+
+		b2ShapeType type;
+
+	};
+	class PhysObject {
+		b2BodyId b2_id;
+
+	protected:
+
+	public:
+		PhysObject( b2WorldId world, PhysSettings& settings );
+
+		~PhysObject();
+
+		const b2BodyId getID() const;
+		sf::Vector2f getPosition() const;
+		sf::Vector2f getVelocity() const;
+		sf::Angle getRotation() const;
+		sf::Angle getAngularVelocity() const;
+
+		void applyImpulse( sf::Vector2f impulse );
+		void applyImpulseAt( sf::Vector2f impulse, sf::Vector2f local_pos );
+
+		void setPosition( sf::Vector2f pos );
+		void setVelocity( sf::Vector2f vel );
+		void setRotation( sf::Angle rotation );
+		void setAngularVelocity( sf::Angle r_vels );
+
+		void wake();
+		void sleep();
+	};
+};
---
+++ src/phys_object.cpp
@@ -0,0 +1,88 @@
+#include "include/phys_object.hpp"
+#include "SFML/Graphics/Transform.hpp"
+#include "SFML/System/Angle.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "box2d/box2d.h"
+#include "box2d/math_functions.h"
+#include "include/engine.hpp"
+#include <cassert>
+
+using Arcade::PhysSettings;
+using Arcade::PhysObject;
+
+PhysObject::PhysObject( b2WorldId world, PhysSettings& settings ) {
+	assert( b2World_IsValid( world ) && "Passed world is not valid");
+	b2_id = b2CreateBody( world, &settings.def );
+	assert( b2Body_IsValid(b2_id) && "Body failed to init");
+	assert( settings.shape_count > 0 && "Must pass at least one shape!");
+	for( size_t i = 0; i < settings.shape_count; i++ ) {
+		switch( settings.type ) {
+			case b2ShapeType::b2_polygonShape:
+				b2CreatePolygonShape(b2_id, &settings.shapes[i], &std::get<b2Polygon>(settings.shape_data[i]));
+				break;
+			case b2_circleShape:
+				b2CreateCircleShape( b2_id, &settings.shapes[i], &std::get<b2Circle>(settings.shape_data[i])  );
+				break;
+			case b2_capsuleShape:
+				b2CreateCapsuleShape( b2_id, &settings.shapes[i], &std::get<b2Capsule>( settings.shape_data[i]) );
+				break;
+			default:
+				assert( "Unsupperted Shape" );
+        }
+	}
+}
+
+PhysObject::~PhysObject() {
+	b2DestroyBody( b2_id );
+}
+
+const b2BodyId PhysObject::getID() const {
+	return b2_id;
+}
+
+sf::Vector2f PhysObject::getPosition() const {
+	return toSFMLVector( b2Body_GetPosition(b2_id) );
+}
+sf::Vector2f PhysObject::getVelocity() const {
+	return toSFMLVector( b2Body_GetLinearVelocity(b2_id) );
+}
+
+sf::Angle PhysObject::getRotation() const {
+	return sf::radians( b2Rot_GetAngle(b2Body_GetRotation(b2_id)) );
+}
+sf::Angle PhysObject::getAngularVelocity() const {
+	return sf::radians( b2Body_GetAngularVelocity( b2_id ) );
+}
+
+/**
+	\brief Applies an impulse to the body at the center
+	\param impulse The vector of impulse
+*/
+void PhysObject::applyImpulse( sf::Vector2f impulse ) {
+	b2Body_ApplyLinearImpulseToCenter(b2_id, toB2DVector(impulse), true);
+}
+/**
+	\brief Applies an impulse to a point on the body.
+	\param impulse The vector of impulse
+	\param local_pos The position to apply the impluse to, relative to the shape. Does not account for rotation
+*/
+void PhysObject::applyImpulseAt( sf::Vector2f impulse, sf::Vector2f local_pos ) {
+	sf::Transform t;
+	t.translate( getPosition() );
+	t.rotate( getRotation() );
+	auto p  = t.transformPoint( local_pos );
+	b2Body_ApplyLinearImpulse(b2_id, toB2DVector(impulse), toB2DVector(p), true);
+}
+
+void PhysObject::setPosition( sf::Vector2f pos ) {
+	b2Body_SetTransform(b2_id, toB2DVector(pos), b2Body_GetRotation(b2_id));
+}
+void PhysObject::setVelocity( sf::Vector2f vel ) {
+	b2Body_SetLinearVelocity(b2_id, toB2DVector(vel));
+}
+void PhysObject::setRotation( sf::Angle rotation ) {
+	b2Body_SetTransform(b2_id, b2Body_GetPosition(b2_id), toB2DAngle(rotation));
+}
+void PhysObject::setAngularVelocity( sf::Angle r_vel ) {
+	b2Body_SetAngularVelocity(b2_id, r_vel.asRadians());
+}
